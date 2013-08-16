fb6ab50bdf807229355b046146ac1649a452dab6
Bjorn Barrefors <barrefors@gmail.com>
2013-08-16 11:48:47 -0500

    Updated data encoding

--- subscribe.py
+++ subscribe.py
@@ -1,20 +1,43 @@
 #!/usr/bin/python
 
 import re
+import urllib
 import urllib2
 import json
+import sys
 
 if __name__ == '__main__':
     # node = UNL
     # data = some small set not at UNL
     # 
-    node = "T2_US_UNL"
-    dataset = "/Pyquen_WToMuNu_TuneZ2_5023GeV_pythia6/HiWinter13-pa_STARTHI53_V25-v2/GEN-SIM-RECO"
+    dataset = '/Pyquen_WToMuNu_TuneZ2_5023GeV_pythia6/HiWinter13-pa_STARTHI53_V25-v2/GEN-SIM-RECO'
     phedex_call = "http://cmsweb.cern.ch/phedex/datasvc/xml/prod/data?dataset=" + dataset
     try:
-        data = urllib2.urlopen(phedex_call)
+        data_response = urllib2.urlopen(phedex_call)
     except:
-        # retry?
-    subscription = "http://cmsweb.cern.ch/phedex/datasvc/xml/prod/subscription?node=" + node + "data=" + data
-    response = urllib2.urlopen(subscription)
-    print response
+        print "Failed phedex call"
+        sys.exit()
+        #data_response = urllib2.urlopen(phedex_call)
+    
+    node = 'T2_US_Nebraska'
+    xml_data = data_response.read()
+    level = 'dataset'
+    priority = 'low'
+    move = 'n'
+    static = 'n'
+    custodial = 'n'
+    request_only = 'n'
+    values = { 'node=' : node, 'data' : xml_data, 'level' : level,
+               'priority' : priority, 'move' : move, 'static' : static,
+               'custodial' : custodial, 'request_only' : request_only }
+    data = urllib.urlencode(values)
+    url = 'http://cmsweb.cern.ch/phedex/datasvc/xml/prod/subscription'
+    request = urllib2.Request(url, data)
+    try:
+        sub_response = urllib2.urlopen(request)
+    except:
+        print "Failed subscription call"
+        sys.exit()
+
+    sub_status = sub_response.read()
+    print sub_status
