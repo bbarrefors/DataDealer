bf5c3224b9ec6764f294fbe2215287ec8ef7000c
Bjorn Barrefors <barrefors@gmail.com>
2013-09-12 13:19:16 -0500

    Improved logging and added file locking

--- PhEDExUDPListener.py
+++ PhEDExUDPListener.py
@@ -21,24 +21,44 @@ import json
 import time
 import datetime
 import sqlite3 as lite
-from multiprocessing import Manager, Process, Pool
+from multiprocessing import Manager, Process, Pool, Lock
 
 SET_ACCESS = 200
 TOTAL_BUDGET = 40000
 TIME_FRAME = 72
 BUDGET_TIME_FRAME = 24
 
-def datasetSize(dataset):
+def subscribe(dataset, l):
+    ID = "Subscribe"
+    fs = open('/home/bockelman/barrefors/data.log', 'a')
+    l.acquire()
+    fs.write(str(datetime.datetime.now()) + " " + str(ID) + ": Subscribe dataset " + str(dataset) + "\n")
+    l.release()
+    cur.execute('INSERT OR IGNORE INTO DontMove VALUES(?)', [dataset])
+    timestamp = datetime.datetime.now()
+    delta = datetime.timedelta(hours=BUDGET_TIME_FRAME)
+    expiration = timestamp + delta
+    cur.execute('INSERT INTO Budget VALUES(?,?,?)', (dataset, int(dataset_size), expiration))
+    fs.close()
+    return 1
+
+def datasetSize(dataset, l):
     """
     _datasetSize_
     
     Accumulate all block sizes and calculate total size of dataset in GB.
     In case of error in PhEDEx call return 0.
     """
+    ID = "DatasetSize"
+    fs = open('/home/bockelman/barrefors/data.log', 'a')
     phedex_call = "http://cmsweb.cern.ch/phedex/datasvc/json/prod/data?dataset=" + str(dataset)
     try:
         response = urllib2.urlopen(phedex_call)
     except:
+        l.acquire()
+        fs.write(str(datetime.datetime.now()) + " " + str(ID) + ": Couldn't get dataset " + str(dataset) + " size\n")
+        l.release()
+        fs.close()
         return 0
     json_data = json.load(response)
     dataset = json_data.get('phedex').get('dbs')[0].get('dataset')[0].get('block')
@@ -47,9 +67,13 @@ def datasetSize(dataset):
         size_dataset += block.get('bytes')
 
     size_dataset = size_dataset / 10**9
+    l.acquire()
+    fs.write(str(datetime.datetime.now()) + " " + str(ID) + ": Dataset " + str(dataset) + " size is " + str(size_dataset) + "\n")
+    l.release()
+    fs.close()
     return int(size_dataset)
 
-def availableSpace():
+def availableSpace(l):
     """
     _availableSpace_
 
@@ -57,14 +81,20 @@ def availableSpace():
     Need to have at least 10% free at all times so return the space 
     available in GB to use without reaching 90% capacity.
     """
+    ID = "PhedexCheck"
+    fs = open('/home/bockelman/barrefors/data.log', 'a')
     info = os.statvfs("/mnt/hadoop")
     total = (info.f_blocks * info.f_bsize) / (1024**3)
     free = (info.f_bfree * info.f_bsize) / (1024**3)
     minimum_free = total*(0.1)
     available_space = free - minimum_free
+    l.acquire()
+    fs.write(str(datetime.datetime.now()) + " " + str(ID) + ": Phedex available space is " + str(available_space) + "\n")
+    l.release()
+    fs.close()
     return int(avail_space_util)
 
-def spaceCheck(dataset):
+def spaceCheck(dataset, l):
     """
     _spaceCheck_
 
@@ -72,24 +102,31 @@ def spaceCheck(dataset):
     without going over the space limit.
     Return 0 fail and size of dataset if possible.
     """
+    ID = "SpaceCheck"
+    fs = open('/home/bockelman/barrefors/data.log', 'a')
     dataset_size = datasetSize(dataset)
     if (size_dataset == 0):
+        fs.close()
         return 0
     else:
         available_space = availableSpace()
         if (phedex_avail_util >= size_dataset):
+            fs.close()
             return int(size_dataset)
         else:
+            fs.close()
             return 0
+    fs.close()
     return 0
 
-def subscriptionDecision():
+def subscriptionDecision(l):
     """
     _subscriptionDecision_
 
     Suggest subscription if a set have been accesses more than SET_ACCESS 
     and moving the set will not fill the new node more than 90%.
     """
+    ID = "Decision"
     fs = open('/home/bockelman/barrefors/data.log', 'a')
     con = lite.connect("/home/bockelman/barrefors/dataset_cache.db")
     with con:
@@ -100,7 +137,9 @@ def subscriptionDecision():
             if row == None:
                 break
             dataset = row[0]
-            fs.write(str(datetime.datetime.now()) + " Suggested data set: " + str(dataset) + " because it had " + str(setAccess) + " set accesses.\n")
+            l.acquire()
+            fs.write(str(datetime.datetime.now()) + " " + str(ID) + ": Dataset " + str(dataset) + " have " + str(setAccess) + " set accesses\n")
+            l.release()
             setAccess = row[1]
             cur.execute('SELECT * FROM DontMove WHERE Dataset=?', [dataset])
             row = cur.fetchone()
@@ -114,21 +153,17 @@ def subscriptionDecision():
                 if row == None:
                     break
                 budget += row[1]
-            dataset_size = spaceCheck(str(dataset))
+            dataset_size = spaceCheck(str(dataset), l)
             if (budget + dataset_size > TOTAL_BUDGET):
                 break
             if (not (size == 0)):
-                fs.write(str(datetime.datetime.now()) + " Move data set: " + str(dataset) + " because it had " + str(setAccess) + " set accesses.\n")
-                cur.execute('INSERT OR IGNORE INTO DontMove VALUES(?)', [dataset])
-                timestamp = datetime.datetime.now()
-                delta = datetime.timedelta(hours=BUDGET_TIME_FRAME)
-                expiration = timestamp + delta
-                cur.execute('INSERT INTO Budget VALUES(?,?,?)', (dataset, int(dataset_size), expiration))
+                # TODO : Check if subscription succeeded
+                subscribe(str(dataset), l)
     con.close()
     fs.close()
     return 1
 
-def update():
+def update(l):
     """
     _update_
 
@@ -136,6 +171,8 @@ def update():
     Update SetCount to reflect database after deletions.
     Delete sets from SetCount if count is 0 or less.
     """
+    ID = "Update"
+    fs = open('/home/bockelman/barrefors/data.log', 'a')
     con = lite.connect("/home/bockelman/barrefors/dataset_cache.db")
     with con:
         cur = con.cursor()
@@ -153,11 +190,15 @@ def update():
         minCount = 1
         cur.execute('DELETE FROM SetCount WHERE Count<?', [minCount])
         cur.execute('DELETE FROM UnknownSet WHERE Expiration<?', [datetime.datetime.now()])
-        cur.execute('DELETE FROM Budget WHERE Expiration<?', [datetime.datetime.now()])
+        #cur.execute('DELETE FROM Budget WHERE Expiration<?', [datetime.datetime.now()])
     con.close()
+    l.acquire()
+    fs.write(str(datetime.datetime.now()) + " " + str(ID) + ": Done updating database\n")
+    l.release()
+    fs.close()
     return 1
 
-def janitor():
+def janitor(l):
     """
     _janitor_
     
@@ -165,16 +206,22 @@ def janitor():
     The janitor is in charge of cleaning out expired 
     entries in the database and suggest subscriptions.
     """
-    Run every hour
+    ID = "Janitor"
+    # Run every hour
     while True:
-        time.sleep(3600)
+        time.sleep(360)
+        fs = open('/home/bockelman/barrefors/data.log', 'a')
+        l.acquire()
+        fs.write(str(datetime.datetime.now()) + " " + str(ID) + ": Running hourly routine\n")
+        l.release()
         # Update database, delete entries older than 12h
-        update()
+        update(l)
         # Check if should make subscriptions
-        subscriptionDecision()
+        subscriptionDecision(l)
+        fs.close()
     return 1
 
-def dataHandler(d):
+def dataHandler(d, l):
     """
     _dataHandler_
 
@@ -182,6 +229,8 @@ def dataHandler(d):
     to insert dataset accesses in database.
     Dataset may not exist, record this as unknown.
     """
+    ID = "Worker"
+    fs = open('/home/bockelman/barrefors/data.log', 'a')
     con = lite.connect("/home/bockelman/barrefors/dataset_cache.db")
     lfn = str(d['file_lfn'])
     with con:
@@ -218,16 +267,23 @@ def dataHandler(d):
                 else:
                     in_count = 1
                     cur.execute('INSERT INTO SetCount VALUES(?,?)', (dataset, in_count))
+                l.acquire()
+                fs.write(str(datetime.datetime.now()) + " " + str(ID) + ": Access to dataset " + str(dataset) + " from file " + str(lfn) + " added to datatbase\n")
+                l.release()
             else:
                 # Unknown log
                 delta = datetime.timedelta(hours=TIME_FRAME)
                 timestamp = datetime.datetime.now()
                 dataset = "UNKNOWN"
                 cur.execute('INSERT OR IGNORE INTO UnknownSet VALUES(?,?,?)', (lfn, dataset, timestamp))
+                l.acquire()
+                fs.write(str(datetime.datetime.now()) + " " + str(ID) + ": Access to unknown dataset with file " + str(lfn) + " added to database\n")
+                l.release()
+    fs.close()
     con.close()
     return 1
 
-def work(q):
+def work(q, l):
     """
     _work_
     
@@ -235,7 +291,7 @@ def work(q):
     """
     while True:
         d = q.get()
-        dataHandler(d)
+        dataHandler(d, l)
 
 def dataParser(data):
     """
@@ -264,6 +320,7 @@ if __name__ == '__main__':
     Spawn worker processes and janitor.
     Recieve UDP packets and send to parser and then distribute to workers.
     """
+    ID = "Main"
     config_f = open('listener.conf', 'r')
     for line in config_f:
         if re.match("set_access", line):
@@ -296,26 +353,32 @@ if __name__ == '__main__':
         cur.execute('INSERT OR IGNORE INTO DontMove VALUES(?)', [dataset])
     
     # Spawn worker processes that will parse data and insert into database
+    lock = Lock()
     pool = Pool(processes=4)
     manager = Manager()
     queue = manager.Queue()
 
     # Spawn process that to clean out database and make reports every 1h
-    process = Process(target=janitor, args=())
+    process = Process(target=janitor, args=(lock,))
     process.start()
-    workers = pool.apply_async(work, (queue,))
+    workers = pool.apply_async(work, (queue, lock))
 
     # UDP packets containing information about file access
     UDPSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     listen_addr = ("0.0.0.0", 9345)
     UDPSock.bind(listen_addr)
     buf = 64*1024
-
+    
+    fs = open('/home/bockelman/barrefors/data.log', 'a')
+    
     # Listen for UDP packets
     try:
         while True:
             data,addr = UDPSock.recvfrom(buf)
             dictionary = dataParser(data)
+            lock.acquire()
+            fs.write(str(datetime.datetime.now()) + " " + str(ID) + ": UDP packet received\n")
+            lock.release()            
             queue.put(dictionary)
 
     #Close everything if program is interupted
@@ -323,4 +386,5 @@ if __name__ == '__main__':
         UDPSock.close()
         pool.close()
         pool.join()
+        fs.close()
         process.join()
