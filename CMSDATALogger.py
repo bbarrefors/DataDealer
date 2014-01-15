efb66129c2d26c4a0266ba6d5e0802bfde102f53
Bjorn Barrefors <barrefors@gmail.com>
2014-01-15 16:52:05 -0600

    Logger: Fixed comments. Variable names. Exception handling

--- CMSDATALogger.py
+++ CMSDATALogger.py
@@ -3,80 +3,91 @@
 """
 _CMSDATALogger_
 
-Print log and error messages from other modules to log file.
-
 Created by Bjorn Barrefors on 22/9/2013
 for CMSDATA (CMS Data Analyzer and Transfer Agent)
 
 Holland Computing Center - University of Nebraska-Lincoln
 """
+__author__ =  'Bjorn Barrefors'
+__version__ = '0.3'
+
+import sys
+import os
+import datetime
+
+
 ################################################################################
 #                                                                              #
 #                       C M S D A T A   L O G G E R                            #
 #                                                                              #
 ################################################################################
 
-import sys
-import os.path
-import datetime
-import fcntl
+class CMSDATALogger:
+    """
+    _CMSDATALogger_
+    
+    Print log and error messages from other modules to log file
+    Esceptions need to be caught by the caller, see the __main__
+    function for implementation of error handling
 
-LOG_PATH = '/grid_home/cmsphedex/'
-#LOG_PATH = '/home/bockelman/barrefors/cmsdata/'
-#LOG_PATH = 'home/barrefors/cmsdata/'
-LOG_FILE = 'cmsdata.log'
+    Class variables:
+    log_file -- File descriptor for log file
+    """
+    def __init__(self, log_path='/home/barrefors/cmsdata/test/', file_name='cmsdata.log'):
+        """
+        Open log file filedescriptor
+
+        Keyword arguments:
+        log_path  -- Path to log file
+        file_name -- Name of log file
+        """
+        # Alternative log paths:
+        # /home/barrefors/cmsdata/test/
+        # /grid_home/cmsphedex/
+        # /home/bockelman/barrefors/cmsdata/
+        # /root/test/cmsdata/
+
+        if not os.path.isdir(log_path):
+            os.makedirs(log_path)
+        self.log_file = open(log_path + file_name, 'a')
+
+
+    ################################################################################
+    #                                                                              #
+    #                                L O G                                         #
+    #                                                                              #
+    ################################################################################
+
+    def log(self, name, msg):
+        """
+        _log_
+
+        Function is fairly straight forward, name is the name of 
+        module printing log message.
+        """
+        self.log_file.write("LOG: %s %s: %s\n" % (str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), str(name), str(msg)))
+
+
+    ################################################################################
+    #                                                                              #
+    #                                E R R O R                                     #
+    #                                                                              #
+    ################################################################################
+
+    def error(self, name, msg):
+        """
+        _error_
+
+        Identical to log but prints out error messages to the same file.
+        """
+        self.log_file.write("ERROR: %s %s: %s\n" % (str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), str(name), str(msg)))
+
 
 ################################################################################
 #                                                                              #
-#                                L O G                                         #
+#                                  M A I N                                     #
 #                                                                              #
 ################################################################################
-
-def log(name, msg):
-    """
-    _log_
-
-    Function is fairly straight forward, pass the ID of module
-    calling logging and log message.
-    Print out log message msg to the standard log file.
-    """
-    if os.path.exists(LOG_PATH):
-        log_file = open(LOG_PATH + LOG_FILE, 'a')
-    else:
-        return 1
-    try:
-        fcntl.flock(log_file, fcntl.LOCK_EX|fcntl.LOCK_NB)
-    except IOError:
-        fcntl.flock(log_file, fcntl.LOCK_EX)
-    log_file.write("LOG: %s %s: %s\n" % (str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), str(name), str(msg)))
-    fcntl.lockf(log_file, fcntl.LOCK_UN)
-    log_file.close()
-    return 0
-
-################################################################################
-#                                                                              #
-#                                E R R O R                                     #
-#                                                                              #
-################################################################################
-
-def error(name, msg):
-    """
-    _error_
-
-    Identical to log but prints out error messages to the same file.
-    """
-    if os.path.exists(LOG_PATH):
-        log_file = open(LOG_PATH + LOG_FILE, 'a')
-    else:
-        return 1
-    try:
-        fcntl.flock(log_file, fcntl.LOCK_EX|fcntl.LOCK_NB)
-    except IOError:
-        fcntl.flock(log_file, fcntl.LOCK_EX)
-    log_file.write("ERROR: %s %s: %s\n" % (str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), str(name), str(msg)))
-    fcntl.lockf(log_file, fcntl.LOCK_UN)
-    log_file.close()
-    return 0
 
 if __name__ == '__main__':
     """
@@ -84,4 +95,15 @@ if __name__ == '__main__':
     
     For testing purpose only.
     """
-    sys.exit(error("name", "This is a message"))
+    try:
+        my_logger = CMSDATALogger()
+        my_logger.error("Bjorn", "Test message 2")
+    except IOError, e:
+        # Couldn't open file
+        print "Couldn\'t access log file. Reason: %s" % (e,)
+        sys.exit(1)
+    except OSError, e:
+        # Couldn't create path to log file
+        print "Couldn\'t create log file. Reason: %s" % (e,)
+        sys.exit(1)
+    sys.exit(0)
