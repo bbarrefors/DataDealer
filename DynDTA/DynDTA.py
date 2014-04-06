5a6958400d44061fa3b71cb8b88401979443c366
Bjorn Barrefors <barrefors@gmail.com>
2014-04-06 17:27:29 -0500

    Added deletion check, tested

--- DynDTA/DynDTA.py
+++ DynDTA/DynDTA.py
@@ -131,13 +131,13 @@ class DynDTA:
             size_TB = self.size(dataset)
             if size_TB == 1000:
                 continue
-            # Check if set was deleted from any of the sites in the last 2 weeks
-            if deleted(dataset, sites):
-                continue
-            # Check if set already exists at site(s)
             i = 0
             current_site = site
             while i < 3:
+                # Check if set was deleted from any of the sites in the last 2 weeks
+                if not (self.deleted(dataset, sites[current_site])):
+                    break
+                # Check if set already exists at site(s)
                 if not (self.replicas(dataset, sites[current_site])):
                     break
                 i += 1
@@ -191,7 +191,7 @@ class DynDTA:
                 continue
             elif (dataset['COLLNAME'].find("/USER") != -1):
                 continue
-            elif (dataset['COLLNAME'].find("/AOD") == -1)):
+            elif (dataset['COLLNAME'].find("/AOD") == -1):
                 continue
             datasets[dataset['COLLNAME']] = dataset['NACC']
             i += 1
@@ -344,18 +344,22 @@ class DynDTA:
     #                                                                          #
     ############################################################################
 
-    def deleted(self, dataset, sites):
+    def deleted(self, dataset, site):
         """
         _deleted_
 
         Check if dataset was deleted from any of the sites by AnalysisOps in the
         last 2 weeks.
         """
-        check, response = self.phedex_api.data(dataset=dataset_block)
+        check, response = self.phedex_api.deletions(node=site, dataset=dataset, request_since='last_30days')
         if check:
-            return subscriptions
+            return False
         try:
-            data = response.get('phedex').get('dbs')[0]
+            data = response.get('phedex').get('dataset')[0]
+        except IndexError, e:
+            return False
+        return True
+
 
 ################################################################################
 #                                                                              #
